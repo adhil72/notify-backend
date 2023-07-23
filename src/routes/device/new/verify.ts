ba0f65457b262250121f7be931c43033c5090599
Adhil <adhil.mhdk28@gmail.com>
2023-07-23 15:20:42 +0530

    get connected clients

--- src/routes/device/new/verify.ts
+++ src/routes/device/new/verify.ts
@@ -3,6 +3,7 @@ import { validateAddToken } from '../../../Helpers/Database/Schemas/Device';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    req.body.access = req.headers.access
     res.send(await validateAddToken(req.body))
 });
 
