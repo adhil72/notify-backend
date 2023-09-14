b72421e068306c314a8a596e7833716c3cd42f09
adhil <adhil.mhdk28@gmail.com>
2023-09-14 08:39:41 +0530

    configured authentication

--- src/Helpers/Database.js
+++ src/Helpers/Database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connect = () => {
-    return mongoose.connect('mongodb://127.0.0.1:27017/visitingcard')
+    return mongoose.connect('mongodb://127.0.0.1:27017/notify')
 }
 
 export {connect}
