9fc36c1c492fe6e4b21af1afd2d5a236d9a670ae
adhil <adhil.mhdk28@gmail.com>
2023-10-14 20:01:37 +0530

    db config

--- src/Helpers/Database.js
+++ src/Helpers/Database.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const connect = () => {
-    return mongoose.connect('mongodb://127.0.0.1:27017/notify')
+    let accessString = process.env.DB
+    return mongoose.connect(accessString?accessString:'')
 }
 
 export {connect}
