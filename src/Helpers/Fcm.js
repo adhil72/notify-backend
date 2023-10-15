7f1443e6b8e3f0d4aa67c9600e3c06c1978e7314
adhil <adhil.mhdk28@gmail.com>
2023-10-15 08:24:20 +0530

    fcm fix

--- src/Helpers/Fcm.js
+++ src/Helpers/Fcm.js
@@ -1,19 +1,6 @@
 import admin from "firebase-admin"
-
-const getEnvValue = (key) => process.env[key]
-var serviceAccount = {
-    "type": getEnvValue("TYPE"),
-    "project_id": getEnvValue("PROJECT_ID"),
-    "private_key_id": getEnvValue("PRIVATE_KEY_ID"),
-    "private_key": getEnvValue("PRIVATE_KEY"),
-    "client_email": getEnvValue("CLIENT_EMAIL"),
-    "client_id": getEnvValue("CLIENT_ID"),
-    "auth_uri": getEnvValue("AUTH_URI"),
-    "token_uri": getEnvValue("TOKEN_URI"),
-    "auth_provider_x509_cert_url": getEnvValue("AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": getEnvValue("CLIENT_X509_CERT_URL"),
-    "universe_domain": getEnvValue("UNIVERSE_DOMAIN")
-}
+import { readFileSync } from 'fs'
+var serviceAccount = JSON.parse(readFileSync(process.cwd() + '/firebase-admin.json', 'utf8'))
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
